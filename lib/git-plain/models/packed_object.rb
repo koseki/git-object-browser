a1bb6b719023efd70f1ca9c42b2392a6b2af1f31
KOSEKI Kengo <koseki@gmail.com>
2012-11-23 12:13:30 +0900

    refactoring

--- lib/git-plain/models/packed_object.rb
+++ lib/git-plain/models/packed_object.rb
@@ -31,45 +31,7 @@ module GitPlain
         @size = header[:size]
 
         if @type == 'OBJ_OFS_DELTA'
-          @delta_offset = header[:delta_offset]
-
-          store = Zlib::Inflate.new
-          buffer = ""
-          while buffer.size < @size
-            rawdata = raw(4096)
-            if rawdata.size == 0
-              raise "inflate error"
-            end
-            buffer << store.inflate(rawdata)
-          end
-          store.close
-
-          tmp = @in
-          @in = StringIO.new(buffer)
-          @base_size  = parse_delta_size
-          @delta_size = parse_delta_size
-          @delta_commands = []
-          while ! @in.eof?
-            cmd = byte
-            if cmd & 0b10000000 != 0
-              (offset, size) = parse_base_offset_and_size(cmd)
-              @delta_commands << { :source => :base, :offset => offset, :size => size }
-            elsif cmd != 0
-              begin
-                data = raw(cmd).encode('UTF-8')
-              rescue Exception
-                data = "(not UTF-8)"
-              end
-              @delta_commands << { :source => :delta, :size => cmd, :data => data }
-            else
-              raise "delta command = 0"
-            end
-          end
-          @in = tmp
-
-          File.open("/tmp/git-plain-buffer", "w") do |io|
-            io << buffer
-          end
+          obj_ofs_delta(header)
         end
 
         self
@@ -86,63 +48,6 @@ module GitPlain
         return header
       end
 
-      def parse_type_and_size
-        hdr      = byte
-        continue = (hdr & 0b10000000)
-        type     = (hdr & 0b01110000) >> 4
-        size     = (hdr & 0b00001111)
-        size_len = 4
-        while continue != 0
-          hdr        = byte
-          continue   = (hdr & 0b10000000)
-          size      += (hdr & 0b01111111) << size_len
-          size_len  += 7
-        end
-        return [type, size]
-      end
-      private :parse_type_and_size
-
-      # delta.h get_delta_hdr_size
-      def parse_delta_size
-        size     = 0
-        size_len = 0
-        begin
-          hdr       = byte
-          continue  = (hdr & 0b10000000)
-          size     += (hdr & 0b01111111) << size_len
-          size_len += 7
-        end while continue != 0
-        return size
-      end
-      private :parse_delta_size
-
-      # unpack-objects.c unpack_delta_entry
-      def parse_delta_offset
-        offset = -1
-        begin
-          hdr        = byte
-          continue   = hdr & 0b10000000
-          low_offset = hdr & 0b01111111
-          offset = ((offset + 1) << 7) | low_offset
-        end while continue != 0
-        return offset
-      end
-      private :parse_delta_offset
-
-      def parse_base_offset_and_size(cmd)
-        offset = size = 0
-        offset  = byte       if cmd & 0b00000001 != 0
-        offset |= byte << 8  if cmd & 0b00000010 != 0
-        offset |= byte << 16 if cmd & 0b00000100 != 0
-        offset |= byte << 24 if cmd & 0b00001000 != 0
-        size    = byte       if cmd & 0b00010000 != 0
-        size   |= byte << 8  if cmd & 0b00100000 != 0
-        size   |= byte << 16 if cmd & 0b01000000 != 0
-        size = 0x10000 if size == 0
-        return [offset,size]
-      end
-      private :parse_base_offset_and_size
-
       def to_hash
         return {
           :type => @type,
@@ -157,6 +62,117 @@ module GitPlain
       def self.path?(relpath)
         return relpath =~ %r{\Aobjects/pack/pack-[0-9a-f]{40}.pack\z}
       end
+
+
+      private
+
+      def obj_ofs_delta(header)
+        @delta_offset = header[:delta_offset]
+        buffer = unpack_delta
+
+        tmp = @in
+        begin
+          @in = StringIO.new(buffer)
+          patch_delta
+        ensure
+          @in = tmp
+        end
+      end
+
+      def patch_delta
+        @base_size  = parse_delta_size
+        @delta_size = parse_delta_size
+        @delta_commands = []
+        while ! @in.eof?
+          delta_command
+        end
+      end
+
+      def delta_command
+        cmd = byte
+        if cmd & 0b10000000 != 0
+          (offset, size) = parse_base_offset_and_size(cmd)
+          @delta_commands << { :source => :base, :offset => offset, :size => size }
+        elsif cmd != 0
+          size = cmd
+          begin
+            data = raw(size).encode('UTF-8')
+          rescue Exception
+            data = "(not UTF-8)"
+          end
+          @delta_commands << { :source => :delta, :size => size, :data => data }
+        else
+          raise "delta command = 0"
+        end
+      end
+
+      def unpack_delta
+        store = Zlib::Inflate.new
+        buffer = ""
+        while buffer.size < @size
+          rawdata = raw(4096)
+          if rawdata.size == 0
+            raise "inflate error"
+          end
+          buffer << store.inflate(rawdata)
+        end
+        store.close
+        buffer
+      end
+
+      def parse_type_and_size
+        hdr      = byte
+        continue = (hdr & 0b10000000)
+        type     = (hdr & 0b01110000) >> 4
+        size     = (hdr & 0b00001111)
+        size_len = 4
+        while continue != 0
+          hdr        = byte
+          continue   = (hdr & 0b10000000)
+          size      += (hdr & 0b01111111) << size_len
+          size_len  += 7
+        end
+        return [type, size]
+      end
+
+      # delta.h get_delta_hdr_size
+      def parse_delta_size
+        size     = 0
+        size_len = 0
+        begin
+          hdr       = byte
+          continue  = (hdr & 0b10000000)
+          size     += (hdr & 0b01111111) << size_len
+          size_len += 7
+        end while continue != 0
+        return size
+      end
+
+      # unpack-objects.c unpack_delta_entry
+      def parse_delta_offset
+        offset = -1
+        begin
+          hdr        = byte
+          continue   = hdr & 0b10000000
+          low_offset = hdr & 0b01111111
+          offset = ((offset + 1) << 7) | low_offset
+        end while continue != 0
+        return offset
+      end
+
+      def parse_base_offset_and_size(cmd)
+        offset = size = 0
+        offset  = byte       if cmd & 0b00000001 != 0
+        offset |= byte << 8  if cmd & 0b00000010 != 0
+        offset |= byte << 16 if cmd & 0b00000100 != 0
+        offset |= byte << 24 if cmd & 0b00001000 != 0
+        size    = byte       if cmd & 0b00010000 != 0
+        size   |= byte << 8  if cmd & 0b00100000 != 0
+        size   |= byte << 16 if cmd & 0b01000000 != 0
+        size = 0x10000 if size == 0
+        return [offset,size]
+      end
+
     end
   end
 end
