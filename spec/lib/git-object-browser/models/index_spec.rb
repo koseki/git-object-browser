dd8c7c6768bdb0ba3c0fe9a0b5c3d7b659edf4fe
KOSEKI Kengo <koseki@gmail.com>
2013-01-14 02:52:19 +0900

    stop using worktree index file

--- spec/lib/git-object-browser/models/index_spec.rb
+++ spec/lib/git-object-browser/models/index_spec.rb
@@ -4,9 +4,10 @@ require 'spec_helper.rb'
 
 module GitObjectBrowser::Models
   describe Index do
-    let(:infile) { File.join(FIXTURES_DIR, 'worktree/_git/index') }
-    let(:input) { File.open(infile) }
+    let(:infile) { '001' }
+    let(:input) { File.open(File.join(FIXTURES_DIR, 'git/indexes', infile)) }
     subject { Index.new(input) }
+    after { input.close if input && ! input.closed? }
 
     it 'should parse header' do
       index = subject.parse.to_hash
@@ -17,6 +18,7 @@ module GitObjectBrowser::Models
     it 'should parse entries' do
       index = subject.parse.to_hash
       entries = index['entries']
+      entries.length.should eq 2
       entries[0]['path'].should eq 'sample-a.txt'
       entries[0]['sha1'].should eq '1d3dc60b5a117054e43741d51e599ff31bb15f9f'
       entries[0]['object_type'].should eq '1000'
