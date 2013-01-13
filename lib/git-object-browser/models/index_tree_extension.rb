e336d4d0d137c1783ff62a7f28668ca529e682bb
KOSEKI Kengo <koseki@gmail.com>
2013-01-14 02:50:21 +0900

    fix index tree extension bug
    the entry_count could be -1.

--- lib/git-object-browser/models/index_tree_extension.rb
+++ lib/git-object-browser/models/index_tree_extension.rb
@@ -21,13 +21,19 @@ module GitObjectBrowser
           entry['path_component'] = find_char "\0"
           entry['entry_count']    = find_char " "
           entry['subtree_count']  = find_char "\n"
-          entry['sha1']           = hex(20)
-          @entries << entry
 
           length += entry['path_component'].bytesize + 1
           length += entry['entry_count'].bytesize + 1
           length += entry['subtree_count'].bytesize + 1
-          length += 20
+
+          entry['entry_count']    = entry['entry_count'].to_i
+          entry['subtree_count']  = entry['subtree_count'].to_i
+
+          if length < @total_length
+            entry['sha1']           = hex(20)
+            length += 20
+          end
+          @entries << entry
         end
 
         self
--- spec/lib/git-object-browser/models/index_tree_extension_spec.rb
+++ spec/lib/git-object-browser/models/index_tree_extension_spec.rb
@@ -3,17 +3,56 @@
 require 'spec_helper.rb'
 
 module GitObjectBrowser::Models
-  describe Index do
-    let(:infile) { File.join(FIXTURES_DIR, 'worktree/_git/index') }
-    let(:input) { File.open(infile) }
-    subject { Index.new(input) }
+  describe IndexTreeExtension do
 
-    it 'should parse tree extensions' do
-      index = subject.parse.to_hash
-      tree = index['extensions'][0]
-      tree['signature'].should eq 'TREE'
-      tree['total_length'].should eq 25
-      tree['entries'][0]['sha1'].should eq 'c36491256978d26c08cd7aa97eee0f5631f96659'
+    let(:input) { File.open(File.join(FIXTURES_DIR, 'git/indexes', infile)) }
+    subject { Index.new(input) }
+    after { input.close if input && ! input.closed? }
+
+    context 'when tree extension exists' do
+      let(:infile) { '001' }
+
+      it 'should parse extension signature and length' do
+        data = subject.parse.to_hash
+        tree = data['extensions'][0]
+        tree['signature'].should eq 'TREE'
+        tree['total_length'].should eq 25
+      end
+
+      it 'should parse entries' do
+        entries = subject.parse.to_hash['extensions'][0]['entries']
+        entries.length.should eq 1
+        entry = entries.first
+        entry['sha1'].should eq 'c36491256978d26c08cd7aa97eee0f5631f96659'
+        entry['entry_count'].should eq 2
+        entry['subtree_count'].should eq 0
+      end
+    end
+
+    context 'when extension includes no entries' do
+      let(:infile) { '002-empty-tree-extension' }
+
+      it 'should parse extension signature and length' do
+        data = subject.parse.to_hash
+        tree = data['extensions'][0]
+        tree['signature'].should eq 'TREE'
+        tree['total_length'].should eq 6
+      end
+
+      it 'should return -1 entry_count' do
+        entries = subject.parse.to_hash['extensions'][0]['entries']
+        entries.length.should eq 1
+        entry = entries.first
+        entry['entry_count'].should eq -1
+        entry['subtree_count'].should eq 0
+        entry['path_component'].should eq ''
+        entry['sha1'].should be_nil
+      end
+
+      it 'return correct hash' do
+        data = subject.parse.to_hash
+        data['sha1'].should eq 'cb9dab99534f6561c467b2fcbbd7cf54a9e8fc05'
+      end
     end
   end
 end
