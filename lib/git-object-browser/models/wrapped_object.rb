e0f46a3af1c3f4827b47107aa9ca3d5b3d80ed2f
KOSEKI Kengo <koseki@gmail.com>
2013-07-15 10:10:03 +0900

    when dumping, root is set to nil

--- lib/git-object-browser/models/wrapped_object.rb
+++ lib/git-object-browser/models/wrapped_object.rb
@@ -23,12 +23,12 @@ module GitObjectBrowser
         self[:object] = obj.to_hash
         self[:root] = root
         self[:path] = path
-        self[:working_dir] = File.basename(File.dirname(root))
+        self[:working_dir] = File.basename(File.dirname(root.to_s))
 
         case(self[:type])
         when 'packed_object'
           sha1 = self[:object][:object][:sha1]
-          unpacked_file = File.join(root, 'objects', sha1[0..1], sha1[2..-1]).to_s
+          unpacked_file = File.join(root.to_s, 'objects', sha1[0..1], sha1[2..-1]).to_s
           self[:unpacked] = File.exist?(unpacked_file)
         when 'pack_index'
           self.merge!(obj.page_data)
